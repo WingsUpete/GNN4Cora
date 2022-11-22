7b2e7299de93650d8dd3d0060a25453da7c1bb00
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 08:19:35 +0000

    Adding tarfile member sanitization to extractall()

--- preprocess/CoraPreprocessor.py
+++ preprocess/CoraPreprocessor.py
@@ -75,7 +75,26 @@ class CoraPreProcessor:
 
         print('> [CoraPreprocessor:prepare_data] Extracting files...')
         with tarfile.open(tgz_path) as f:
-            f.extractall(self.data_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, self.data_dir)
         temp_dir = os.path.join(self.data_dir, 'cora/')
         for file in os.listdir(temp_dir):
             shutil.move(os.path.join(temp_dir, file), self.data_dir)
